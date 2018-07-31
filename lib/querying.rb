f8b8fe13ce72b8533e67a95b91db9fa6c77e002a
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-31 19:33:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
    FROM characters
-   WHERE characters.motto > 0"
+   WHERE LENGTH(characters.motto) > 0"
 end
 
 
