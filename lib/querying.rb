a01a54f2ff4c0e2d1baec199be3083ad9d875dbd
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-31 20:30:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title, MAX(characters.species)
-   FROM series"
+   FROM series
+   INNER JOIN characters
+   ON characters.series_id = series.id
+   WHERE MAX(characters.species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
